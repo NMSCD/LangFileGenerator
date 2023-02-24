30ce27fb63f631be4a87a90206a3926ff8ff693d
Lenni009 <guentherhanslphdyt@gmail.com>
2023-02-24 20:57:14 +0100

    write to file

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 import { parse } from "https://deno.land/x/xml@2.1.0/mod.ts"
 
 const exmlDir = './EXML/';
+const outputDir = './output/';
+const outputFileName = 'Lenni.txt';
 const files = Deno.readDirSync(exmlDir);
 const langData = new Object;
 
 for (const file of files) {
-	if (!file.isFile) continue;
 	const fullFileName = file.name;
+	const fileType = fullFileName.split('.').at(-1);
+	if (!file.isFile || fileType?.toLowerCase() != 'exml') continue;
 	const fileData = Deno.readTextFileSync(exmlDir + fullFileName);
 	const document = parse(fileData);
 	const langElements = document.Data.Property.Property;
@@ -23,8 +26,16 @@ for (const file of files) {
 			const langValue = entry.Property['@value'];
 			if (langValue == null) continue;
 			const language = entry['@name'];
-			langData[langKey][language] = langValue;
+			langData[langKey][language] = langValue.replaceAll('&#xA;', '\n');
 		}
 	}
+	console.log(`Processed ${fullFileName}`);
 }
-console.log(langData)
+const textContent = [];
+for (const key in langData) {
+	textContent.push(key + '\n')
+	Object.values(langData[key]).forEach(item => textContent.push(item + '\n'));
+	textContent.push('\n')
+}
+Deno.writeTextFileSync(outputDir + outputFileName, textContent.join(''))
+console.log("done!");
