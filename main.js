4fdf87c68e71f57a465d8ef5742b515d36a67c4d
Lenni009 <guentherhanslphdyt@gmail.com>
2023-02-25 08:34:06 +0100

    use npm parser

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // import XML parser
-import { parse } from "https://deno.land/x/xml@2.1.0/mod.ts"
+import { XMLParser } from "npm:fast-xml-parser"
 
 // define paths
 const exmlDir = './EXML/';
@@ -14,13 +14,20 @@ Deno.mkdirSync(outputDir, { recursive: true });
 const files = Deno.readDirSync(exmlDir);
 const langData = new Object;
 
+// set up XML parser
+const options = {
+	ignoreAttributes: false,
+	attributeNamePrefix: "@",
+}
+const parser = new XMLParser(options);
+
 // loop through EXML files
 for (const file of files) {
 	const fullFileName = file.name;
 	const fileType = fullFileName.split('.').at(-1);
 	if (!file.isFile || fileType?.toLowerCase() != 'exml') continue;
 	const fileData = Deno.readTextFileSync(exmlDir + fullFileName);
-	const document = parse(fileData);
+	const document = parser.parse(fileData);
 	const langElements = document.Data.Property.Property;
 
 	// loop over TkLocalisationData sections
@@ -29,15 +36,15 @@ for (const file of files) {
 		let langKey;
 
 		// loop over individual lang keys and their assigned values
-		for (let i = 0; i < locEntryData.length; i++) {
-			const entry = locEntryData[i];
-			if (i == 0) {
+		for (let j = 0; j < locEntryData.length; j++) {
+			const entry = locEntryData[j];
+			if (j == 0) {
 				langKey = entry['@value'];
 				langData[langKey] ??= new Object;
 				continue;
 			}
 			const langValue = entry.Property['@value'];
-			if (langValue == null) continue;
+			if (!langValue) continue;
 			const language = entry['@name'];
 			langData[langKey][language] = langValue.replaceAll('&#xA;', '\n');
 		}
